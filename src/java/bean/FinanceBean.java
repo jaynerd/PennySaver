492ea319353cd9be96c73cb995de6c5e95b4dd08
unknown <j4yn3rd@gmail.com>
2018-05-03 10:56:08 +1200

    financeBean added

---
+++ src/java/bean/FinanceBean.java
@@ -0,0 +1,11 @@
+package bean;
+
+import javax.ejb.Stateful;
+
+/**
+ * @author Namyoon j4yn3rd@gmail.com
+ */
+@Stateful
+public class FinanceBean {
+
+}
--- src/java/manager/DataManager.java
+++ src/java/manager/DataManager.java
@@ -1,5 +1,7 @@
 package manager;
 
+import bean.FinanceBean;
+import bean.PersonBean;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,6 +10,20 @@ import java.sql.SQLException;
  * @author Namyoon j4yn3rd@gmail.com
  */
 public class DataManager {
+
+    private String tableName = null;
+    private PersonBean personBean;
+    private FinanceBean financeBean;
+
+    public DataManager(PersonBean personBean) {
+        this.personBean = personBean;
+        connect();
+    }
+
+    public DataManager(FinanceBean financeBean) {
+        this.financeBean = financeBean;
+        connect();
+    }
 
     public void connect() {
         try {
