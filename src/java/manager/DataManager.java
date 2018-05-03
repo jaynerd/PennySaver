f6f8a011fc7df1199d6f8c28a74575bd9557d7b5
unknown <j4yn3rd@gmail.com>
2018-05-04 03:34:09 +1200

    sql updated

--- src/java/manager/DataManager.java
+++ src/java/manager/DataManager.java
@@ -3,8 +3,11 @@ package manager;
 import bean.FinanceBean;
 import bean.PersonBean;
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * @author Namyoon j4yn3rd@gmail.com
@@ -15,26 +18,86 @@ public class DataManager {
     private PersonBean person;
     private FinanceBean finance;
 
+    private Connection connection;
+    private Statement statement = null;
+
     public DataManager(PersonBean person) {
         this.person = person;
+        tableName = "Personal";
         connect();
+        if (!checkExistingTable(tableName)) {
+            this.createPersonalTable();
+            //   wordList = this.getInitialContents();
+            //  this.insertToDatabase(wordList);
+        }
     }
 
     public DataManager(FinanceBean financeBean) {
         this.finance = finance;
+        tableName = "Financial";
         connect();
+        if (!checkExistingTable(tableName)) {
+            this.createFinancialTable();
+            //  wordList = this.getInitialContents();
+            //  this.insertToDatabase(wordList);
+        }
     }
 
     public void connect() {
+
+        String url = "jdbc:derby://localhost:1527/sample";
+
         try {
-            String url = "jdbc:mysql://localhost:3306/mysql";
-            Class.forName("com.mysql.jdbc.Driver");
-            try {
-                Connection con = DriverManager.getConnection(url, "root", "870810");
-            } catch (SQLException ex) {
+            connection = DriverManager.getConnection(url,"jjj","1");
+        } catch (SQLException ex) {
+        }
+
+    }
+
+    private boolean checkExistingTable(String tableName) {
+        boolean flag = false;
+        System.out.println("Database: Checking existing tables.");
+        try {
+            String[] types = {"TABLE"};
+            DatabaseMetaData meta = connection.getMetaData();
+            ResultSet result = meta.getTables(null, null, null, null);
+            while (result.next()) {
+                String exTableName = result.getString("TABLE_NAME");
+                if (exTableName.compareToIgnoreCase(tableName) == 0) {
+                    System.out.println("Database: Table '" + tableName + "' already exists.");
+                    flag = true;
+                }
             }
-        } catch (ClassNotFoundException ex) {
-            ex.printStackTrace();
+            if (result != null) {
+                result.close();
+            }
+        } catch (SQLException ex) {
+        }
+        return flag;
+    }
+
+    private void createPersonalTable() {
+        try {
+            statement = connection.createStatement();
+            statement.executeUpdate("CREATE TABLE " + tableName + " (NAME VARCHAR(20), "
+                    + "AGE INT, SALARY INT, HOURS INT)");
+            System.out.println("Database: The table '" + tableName + "' was successfully created.");
+            statement.close();
+        } catch (SQLException ex) {
+
+        }
+    }
+
+    private void createFinancialTable() {
+        try {
+            statement = connection.createStatement();
+            statement.executeUpdate("CREATE TABLE " + tableName + " (INDEX INT, "
+                    + "FURIGANA VARCHAR(20), KANJI VARCHAR(20), ROMAJI VARCHAR(20), "
+                    + "ENGLISH VARCHAR(20), WRONGCOUNT INT)");
+            System.out.println("Database: The table '" + tableName + "' was successfully created.");
+            statement.close();
+        } catch (SQLException ex) {
+
         }
     }
 }
